020efbbb52e885210fb486cec5daceba6817fd22
Suin Choi <suin6301@gmail.com>
2023-10-11 23:50:28 +0900

    reading both csv file is added on code, file is changed to csv file

--- code_draft.cs
+++ code_draft.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
-
+using System.IO;
+using System.Linq;
 // University class
 public class University
 {
@@ -41,6 +42,38 @@ public class Venue
         VenueName = venueName;
         Capacity = capacity;
         Timeslots = new List<Timeslot>();
+    }
+
+    public static List<Venue> ReadVenuesFromCSV(string filePath)
+    {
+        List<Venue> venues = new List<Venue>();
+        var lines = File.ReadLines(filePath);
+
+        // Skip the first line (column headers)
+        bool firstLine = true;
+
+        foreach (var line in lines)
+        {
+            if (firstLine)
+            {
+                firstLine = false;
+                continue; // Skip the first line
+            }
+
+            var fields = line.Split(',');
+            if (fields.Length >= 4)
+            {
+                int venueId = int.Parse(fields[0]);
+                string venueName = fields[1];
+                string location = fields[2];
+                int capacity = int.Parse(fields[3]);
+
+                Venue venue = new Venue(venueId, location, venueName, capacity);
+                venues.Add(venue);
+            }
+        }
+
+        return venues;
     }
 
     public void BookTimeslot(Timeslot timeslot)
@@ -84,7 +117,7 @@ public class Department
 {
     public string Name { get; }
     public List<Unit> Units { get; }
-// public List<Staff> Staff { get; }
+    // public List<Staff> Staff { get; }
 
     public Department(string name)
     {
@@ -92,7 +125,7 @@ public class Department
         Units = new List<Unit>();
         //Staff = new List<Staff>();
     }
-    
+
     public void AddUnit(Unit unit)
     {
         Units.Add(unit);
@@ -135,9 +168,9 @@ public class Timeslot
 
     public void SetTime(DateTime start, DateTime end)
     {
-       // StartTime = start;
+        // StartTime = start;
         //EndTime = end;
-       // Duration = EndTime - StartTime;
+        // Duration = EndTime - StartTime;
     }
 }
 
@@ -149,13 +182,13 @@ public class Unit
     public int MaxStudents { get; }
     public Unit PrerequisiteUnit { get; }
     public string Requirement { get; }
-    public string AssignedStaff { get; set; }
-   // public List<Student> EnrolledStudents { get; }
+    public string Lecturer { get; set; }
+    // public List<Student> EnrolledStudents { get; }
     public List<Timeslot> AvailableTimeslots { get; }
-    
 
 
-    public Unit(string unitId, string unitName, int unitCredit, int maxStudents, Unit prerequisiteUnit, string requirement)
+
+    public Unit(string unitId, string unitName, string lecturer, int unitCredit, int maxStudents, Unit prerequisiteUnit, string requirement)
     {
         UnitId = unitId;
         UnitName = unitName;
@@ -163,43 +196,53 @@ public class Unit
         MaxStudents = maxStudents;
         PrerequisiteUnit = prerequisiteUnit;
         Requirement = requirement;
-        AssignedStaff = null;
-       // EnrolledStudents = new List<Student>();
+        Lecturer = lecturer;
+        // EnrolledStudents = new List<Student>();
         AvailableTimeslots = new List<Timeslot>();
     }
-    static List<Unit> ReadUnitsFromCSV(string filePath)
+    public static List<Unit> ReadUnitsFromCSV(string filePath)
     {
         List<Unit> units = new List<Unit>();
         var lines = File.ReadLines(filePath);
 
-        foreach(var line in lines.Skip(1)) // Skip the header line
-    {
-            var fields = line.Split(',');
-            if (fields.Length >= 2) // CSV 파일에서 "Unit Code"와 "Unit Title" 필드만 사용
-            {
-                string unitId = fields[0]; // "Unit Code"를 "unitId"로 매핑
-                string unitName = fields[1]; // "Unit Title"을 "unitName"으로 매핑
+        // Skip the first line (column headers)
+        bool firstLine = true;
 
-                // 나머지 필드는 필요에 따라 파싱할 수 있습니다.
+        foreach (var line in lines)
+        {
+            if (firstLine)
+            {
+                firstLine = false;
+                continue; // Skip the first line
+            }
+
+            var fields = line.Split(',');
+            if (fields.Length >= 3)
+            {
+                string unitId = fields[0];
+                string unitName = fields[1];
+                string lecturer = fields[2];
+                // Parse other fields as needed.
                 int unitCredit = 0;
                 int maxStudents = 0;
 
-                Unit unit = new Unit(unitId, unitName, unitCredit, maxStudents, null, null);
+                Unit unit = new Unit(unitId, unitName, lecturer, unitCredit, maxStudents, null, null);
                 units.Add(unit);
             }
         }
 
+
         return units;
     }
 
-    public void AssignStaff(string staff)
+    public void AssignLecturer(string lecturer)
     {
-        AssignedStaff = staff;
+        Lecturer = lecturer;
     }
 
-    public void UnassignStaff()
+    public void UnassignLecturer()
     {
-        AssignedStaff = null;
+        Lecturer = null;
     }
 
     public void AddTimeslot(Timeslot timeslot)
@@ -232,28 +275,40 @@ class Program
 {
     static void Main()
     {
-        University university = new University("Sample University");
+        University university = new University("Curtin University");
 
         // Create a department
         Department computerScience = new Department("Computer Science");
 
         // Create a venue
-        Venue venue1 = new Venue(1, "Campus A", "Lecture Hall 101", 150);
+        List<Unit> units = Unit.ReadUnitsFromCSV("unitsample.csv");
+        List<Venue> venues = Venue.ReadVenuesFromCSV("venueSample.csv");
 
+        // Add the units to the department
+        foreach (var unit in units)
+        {
+            computerScience.AddUnit(unit);
+        }
+
+        // Add the venues to the university
+        foreach (var venue in venues)
+        {
+            university.AddVenue(venue);
+        }
         // Create some timeslots
         Timeslot timeslot1 = new Timeslot(1, "Monday", new DateTime(2023, 10, 16, 9, 0, 0), new DateTime(2023, 10, 16, 10, 30, 0));
         Timeslot timeslot2 = new Timeslot(2, "Monday", new DateTime(2023, 10, 16, 10, 45, 0), new DateTime(2023, 10, 16, 12, 15, 0));
 
         // Create a unit
-        Unit computerScience101 = new Unit("unit1001", "Introduction to Computer Science", 3, 30, null, "None");
+        //Unit computerScience101 = new Unit("unit1001", "Introduction to Computer Science", 3, 30, null, "None");
 
         // Assign staff to the unit
         //Staff professorSmith = new Staff("Prof. Smith");
-        computerScience101.AssignStaff("professorSmith");
+        //computerScience101.AssignStaff("professorSmith");
 
         // Add timeslots to the unit
-        computerScience101.AddTimeslot(timeslot1);
-        computerScience101.AddTimeslot(timeslot2);
+        //computerScience101.AddTimeslot(timeslot1);
+        //computerScience101.AddTimeslot(timeslot2);
 
         // Add the department to the university
         university.AddDepartment(computerScience);
@@ -261,10 +316,12 @@ class Program
         // Output some information
         Console.WriteLine("University Name: " + university.Name);
         Console.WriteLine("Department Name: " + computerScience.Name);
-        Console.WriteLine("Venue Name: " + venue1.VenueName);
-        Console.WriteLine("Unit Name: " + computerScience101.UnitName);
+        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine("Venue Name: " + venues[0].VenueName);
+        Console.WriteLine("Venue Name: " + venues[1].VenueName);
+        //Console.WriteLine("Unit Name: " + computerScience101.UnitName);
 
-
+        /*
         Console.WriteLine("Timeslots for the Unit:");
         foreach (var timeslot in computerScience101.AvailableTimeslots)
         {
@@ -273,6 +330,13 @@ class Program
             Console.WriteLine("Start Time: " + timeslot.StartTime);
             Console.WriteLine("End Time: " + timeslot.EndTime);
             Console.WriteLine();
+        }*/
+        // For each unit, you can work with available timeslots, assign staff, etc.
+        Console.WriteLine("--------------------------------------------");
+        foreach (var unit in units)
+        {
+            Console.WriteLine("Unit Name: " + unit.UnitName);
+            Console.WriteLine("Unit Lecturer: " + unit.Lecturer);
         }
     }
 }
