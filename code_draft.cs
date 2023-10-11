16e73717bf351561bfdb2a686a095e87f290238f
Suin Choi <suin6301@gmail.com>
2023-10-11 16:33:56 +0900

    the main is added for testing

--- code_draft.cs
+++ code_draft.cs
@@ -84,15 +84,15 @@ public class Department
 {
     public string Name { get; }
     public List<Unit> Units { get; }
-    public List<Staff> Staff { get; }
+// public List<Staff> Staff { get; }
 
     public Department(string name)
     {
         Name = name;
         Units = new List<Unit>();
-        Staff = new List<Staff>();
+        //Staff = new List<Staff>();
     }
-
+    
     public void AddUnit(Unit unit)
     {
         Units.Add(unit);
@@ -135,9 +135,9 @@ public class Timeslot
 
     public void SetTime(DateTime start, DateTime end)
     {
-        StartTime = start;
-        EndTime = end;
-        Duration = EndTime - StartTime;
+       // StartTime = start;
+        //EndTime = end;
+       // Duration = EndTime - StartTime;
     }
 }
 
@@ -149,8 +149,8 @@ public class Unit
     public int MaxStudents { get; }
     public Unit PrerequisiteUnit { get; }
     public string Requirement { get; }
-    public Staff AssignedStaff { get; set; }
-    public List<Student> EnrolledStudents { get; }
+    public string AssignedStaff { get; set; }
+   // public List<Student> EnrolledStudents { get; }
     public List<Timeslot> AvailableTimeslots { get; }
 
     public Unit(int unitId, string unitName, int unitCredit, int maxStudents, Unit prerequisiteUnit, string requirement)
@@ -162,11 +162,11 @@ public class Unit
         PrerequisiteUnit = prerequisiteUnit;
         Requirement = requirement;
         AssignedStaff = null;
-        EnrolledStudents = new List<Student>();
+       // EnrolledStudents = new List<Student>();
         AvailableTimeslots = new List<Timeslot>();
     }
 
-    public void AssignStaff(Staff staff)
+    public void AssignStaff(string staff)
     {
         AssignedStaff = staff;
     }
@@ -202,3 +202,51 @@ public class Unit
     }
 }
 
+class Program
+{
+    static void Main()
+    {
+        University university = new University("Sample University");
+
+        // Create a department
+        Department computerScience = new Department("Computer Science");
+
+        // Create a venue
+        Venue venue1 = new Venue(1, "Campus A", "Lecture Hall 101", 150);
+
+        // Create some timeslots
+        Timeslot timeslot1 = new Timeslot(1, "Monday", new DateTime(2023, 10, 16, 9, 0, 0), new DateTime(2023, 10, 16, 10, 30, 0));
+        Timeslot timeslot2 = new Timeslot(2, "Monday", new DateTime(2023, 10, 16, 10, 45, 0), new DateTime(2023, 10, 16, 12, 15, 0));
+
+        // Create a unit
+        Unit computerScience101 = new Unit(1, "Introduction to Computer Science", 3, 30, null, "None");
+
+        // Assign staff to the unit
+        //Staff professorSmith = new Staff("Prof. Smith");
+        computerScience101.AssignStaff("professorSmith");
+
+        // Add timeslots to the unit
+        computerScience101.AddTimeslot(timeslot1);
+        computerScience101.AddTimeslot(timeslot2);
+
+        // Add the department to the university
+        university.AddDepartment(computerScience);
+
+        // Output some information
+        Console.WriteLine("University Name: " + university.Name);
+        Console.WriteLine("Department Name: " + computerScience.Name);
+        Console.WriteLine("Venue Name: " + venue1.VenueName);
+        Console.WriteLine("Unit Name: " + computerScience101.UnitName);
+
+
+        Console.WriteLine("Timeslots for the Unit:");
+        foreach (var timeslot in computerScience101.AvailableTimeslots)
+        {
+            Console.WriteLine("TimeSlot ID: " + timeslot.TimeSlotId);
+            Console.WriteLine("Day of Week: " + timeslot.DayOfWeek);
+            Console.WriteLine("Start Time: " + timeslot.StartTime);
+            Console.WriteLine("End Time: " + timeslot.EndTime);
+            Console.WriteLine();
+        }
+    }
+}
